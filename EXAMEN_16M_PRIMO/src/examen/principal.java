f43993657d3238d7df91afa9ab94d4ce54749d9d
davis <davis@Portatil_David.mshome.net>
2022-05-16 09:09:03 +0200

    Primer Commit

---
+++ EXAMEN_16M_PRIMO/src/examen/principal.java
@@ -0,0 +1,16 @@
+package examen;
+
+import java.util.Scanner;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Buenos días Ceinmark, soy David Garcia");
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+
+	}
+
+}
