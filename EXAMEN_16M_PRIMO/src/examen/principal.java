5d5e00a2863ec057967f9df253442742822685fd
davis <davis@Portatil_David.mshome.net>
2022-05-16 09:09:52 +0200

    Numero pedido

--- EXAMEN_16M_PRIMO/src/examen/principal.java
+++ EXAMEN_16M_PRIMO/src/examen/principal.java
@@ -7,9 +7,12 @@ public class principal {
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
+		int numero = 0;
 		
 		System.out.println("Buenos días Ceinmark, soy David Garcia");
 		System.out.println("Introduce un número para averiguar si es primo o no: ");
+		
+		numero = Integer.parseInt(entrada.nextLine());
 
 	}
 
